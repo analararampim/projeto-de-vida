2b4f0fe6e9fcfb98a819b2f2e662158ed97c3158
analararampim <ana.rampim@escola.pr.gov.br>
2026-04-22 10:30:29 -0300

    rfre

--- main.js
+++ main.js
@@ -9,4 +9,11 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
     }
 }
+ 
+
+contadores[0].textContent = tempoObjetivo1;
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
